905c0ac7d59052d74cb046a1fa5f1a359f6b3889
LeeJungHoon <gns14585@naver.com>
2023-11-09 21:23:31 +0900

    package.json에 proxy : http://localhost:8080 추가

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -1,3 +1,56 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+
 export function BoardWrite() {
-  return <h1>게시물 작성</h1>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  function handleSubmit() {
+    axios
+      .post("/api/board/add", {
+        title,
+        content,
+        writer: writer,
+      })
+      .then(() => console.log("잘됨"))
+      .catch(() => console.log("안됨"))
+      .finally(() => console.log("끝"));
+  }
+
+  return (
+    <Box>
+      <h1>게시물 작성</h1>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input vlaue={title} onChange={(e) => setTitle(e.target.value)} />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></Textarea>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input vlaue={writer} onChange={(e) => setWriter(e.target.value)} />
+        </FormControl>
+        <Button onClick={handleSubmit} colorScheme="blue">
+          저장
+        </Button>
+      </Box>
+    </Box>
+  );
 }
