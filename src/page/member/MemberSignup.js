3c1d684e1d497b8322b43ac9e52ff7d37dafe9ce
이정훈 <gns14585@naver.com>
2023-11-12 14:03:20 +0900

    암호확인 로직 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -1,4 +1,11 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 
@@ -7,6 +14,16 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
+
+  let submitAvailable = true;
+
+  if (password != passwordCheck) {
+    submitAvailable = false;
+  }
+
+  if (password.length === 0) {
+    submitAvailable = false;
+  }
 
   function handleSubmit() {
     axios
@@ -28,22 +45,24 @@ export function MemberSignup() {
         <Input vlaue={id} onChange={(e) => setId(e.target.value)} />
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={password.length == 0}>
         <FormLabel>password</FormLabel>
         <Input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <FormErrorMessage>암호를 입력해주세요.</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={password != passwordCheck}>
         <FormLabel>password 확인</FormLabel>
         <Input
           type="password"
           value={passwordCheck}
           onChange={(e) => setPasswordCheck(e.target.value)}
         />
+        <FormErrorMessage>암호가 다릅니다.</FormErrorMessage>
       </FormControl>
 
       <FormControl>
@@ -55,7 +74,12 @@ export function MemberSignup() {
         />
       </FormControl>
 
-      <Button onClick={handleSubmit} colorScheme="blue">
+      {/* isDisabled={!submitAvailable} : Input에 값을 넣지 않으면 가입버튼이 안눌리게 처리 */}
+      <Button
+        isDisabled={!submitAvailable}
+        onClick={handleSubmit}
+        colorScheme="blue"
+      >
         가입
       </Button>
     </Box>
