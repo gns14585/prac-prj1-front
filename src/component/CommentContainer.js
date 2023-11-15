9e49fa5c35e629b6a981efec2d4dfeb954767bc2
이정훈 <gns14585@naver.com>
2023-11-15 19:47:33 +0900

    댓글 쓰기

---
+++ src/component/CommentContainer.js
@@ -0,0 +1,34 @@
+import { Box, Button, Input, Textarea } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+
+function CommentForm({ boardId }) {
+  const [comment, setComment] = useState("");
+
+  function handleSubmit() {
+    axios.post("/api/comment/add", {
+      boardId, // 게시물 id
+      comment, // 댓글
+    });
+  }
+
+  return (
+    <Box>
+      <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+      <Button onClick={handleSubmit}>쓰기</Button>
+    </Box>
+  );
+}
+
+function CommentList() {
+  return <Box>댓글리스트</Box>;
+}
+
+export function CommentContainer({ boardId }) {
+  return (
+    <Box>
+      <CommentForm boardId={boardId} />
+      <CommentList />
+    </Box>
+  );
+}
