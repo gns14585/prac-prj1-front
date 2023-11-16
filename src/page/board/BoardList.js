d2dcae7e8efdf8cdca10299feee954f6d2cf6d8f
이정훈 <gns14585@naver.com>
2023-11-16 22:13:29 +0900

    게시물 리스트에 댓글 수 출력

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
+  Badge,
   Box,
   Spinner,
   Table,
@@ -11,6 +12,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ChatIcon } from "@chakra-ui/icons";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
@@ -50,7 +52,16 @@ export function BoardList() {
                 onClick={() => navigate("/board/" + board.id)}
               >
                 <Td>{board.id}</Td>
-                <Td>{board.title}</Td>
+                <Td>
+                  {board.title}
+                  {/* 해당 게시물의 댓글수 표현 , 0개는 표시되지 않음 */}
+                  {board.countComment > 0 && (
+                    <Badge>
+                      <ChatIcon />
+                      {board.countComment}
+                    </Badge>
+                  )}
+                </Td>
                 <Td>{board.nickName}</Td>
                 <Td>{board.inserted}</Td>
               </Tr>
