66bb6a3b49f49fc0273e831ac3712bf119901ecb
이정훈 <gns14585@naver.com>
2023-11-16 20:32:28 +0900

    수정버튼 클릭시 TextArea 표시 취소버튼 클릭시 TextArea 숨기기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { BoardWrite } from "./page/board/BoardWrite";
+import { BoardList } from "./page/board/BoardList";
 import { HomeLayout } from "./layout/HomeLayout";
-import { BoardList } from "./page/BoardList";
-import { BoardWrite } from "./page/BoardWrite";
-import { BoardView } from "./page/BoardView";
+import { BoardView } from "./page/board/BoardView";
+import { BoardEdit } from "./page/board/BoardEdit";
+import { MemberSignup } from "./page/member/MemberSignup";
+import { MemberList } from "./page/member/MemberList";
+import { MemberView } from "./page/member/MemberView";
+import { MemberEdit } from "./page/member/MemberEdit";
+import { MemberLogin } from "./page/member/MemberLogin";
+import axios from "axios";
+import LoginProvider from "./component/LoginProvider";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -16,12 +24,22 @@ const routes = createBrowserRouter(
       <Route index element={<BoardList />} />
       <Route path="write" element={<BoardWrite />} />
       <Route path="board/:id" element={<BoardView />} />
+      <Route path="edit/:id" element={<BoardEdit />}></Route>
+      <Route path="signup" element={<MemberSignup />} />
+      <Route path="member/list" element={<MemberList />} />
+      <Route path="member" element={<MemberView />} />
+      <Route path="member/edit" element={<MemberEdit />} />
+      <Route path="login" element={<MemberLogin />} />
     </Route>,
   ),
 );
 
 function App(props) {
-  return <RouterProvider router={routes} />;
+  return (
+    <LoginProvider>
+      <RouterProvider router={routes} />
+    </LoginProvider>
+  );
 }
 
 export default App;
