a1534381bd6036dc9a0abfe0950b8635d248f283
LeeJungHoon <gns14585@naver.com>
2023-11-21 16:40:07 +0900

    파일 전송 추가 중

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -14,6 +14,7 @@ import { useNavigate } from "react-router-dom";
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [file, setFile] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const navigate = useNavigate();
 
@@ -23,9 +24,10 @@ export function BoardWrite() {
     setIsSubmitting(true);
 
     axios
-      .post("/api/board/add", {
+      .postForm("/api/board/add", {
         title,
         content,
+        file,
       })
       .then(() => {
         toast({
@@ -66,6 +68,15 @@ export function BoardWrite() {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           ></Textarea>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>이미지</FormLabel>
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
         </FormControl>
 
         <Button
